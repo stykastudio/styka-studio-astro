60dc2f9a3ab97f9c4f3c0fcc4f25762c4c594fb2
Deepak Vemula <deepakvemula9@gmail.com>
2024-02-29 21:26:04 +0530

    added transitions.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -18,6 +18,21 @@ module.exports = {
 				DEFAULT: "0 2px 4px var(--tw-shadow-color)",
 				lg: "0 8px 16px var(--tw-shadow-color)",
 			},
+			keyframes: {
+				slidein: {
+					from: {
+						opacity: "0",
+						transform: "translateY(-10px)",
+					},
+					to: {
+						opacity: "1",
+						transform: "translateY(0)",
+					},
+				},
+			},
+			animation: {
+				slidein: "slidein 1s ease var(--slidein-delay, 0) forwards",
+			},
 		},
 		colors: {
 			transparent: "transparent",
